261a0913c930f60dd23a61170bbff67ce9a0d4f4
stevenjlance <44912347+stevenjlance@users.noreply.github.com>
2021-10-31 18:20:05 -0400

    Added data

--- src/data.js
+++ src/data.js
@@ -1,26 +1,214 @@
-// You can use this example data if you're following the lecture guide. 
-let data = {
-  "artist": "My favorite singer",
-  "album": "Their best album",
-  "image": "albumCover.png",
-  "tracks": [
-    {
-      "id": 1,
-      "title": "Opening Song",
-      "duration": "2:31",
-      "comment": "makes me want to dance"
-    },
-    {
-      "id": 2,
-      "title": "Second Song",
-      "duration": "3:12",
-      "comment": "always skip this one"
-    },
-    {
-      "id": 3,
-      "title": "Third Song",
-      "duration": "5:33",
-      "comment": "my favorite!"
-    }
-  ]
-}
+// You can use this example data if you're following the lecture guide.
+let data = [
+  {
+    artist: "Beyonce",
+    album: "I Am Sasha Fierce",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/9/96/I_Am..._Sasha_Fierce.png",
+    tracks: [
+      {
+        id: 1,
+        title: "If I Were a Boy",
+        duration: "4:09"
+      },
+      {
+        id: 2,
+        title: "Halo",
+        duration: "4:21"
+      },
+      {
+        id: 3,
+        title: "Disappear",
+        duration: "4:27"
+      },
+      {
+        id: 4,
+        title: "Broken-Hearted Girl",
+        duration: "4:37"
+      },
+      {
+        id: 5,
+        title: "Ave Maria",
+        duration: "3:42"
+      },
+      {
+        id: 6,
+        title: "Satellites",
+        duration: "3:06"
+      },
+      {
+        id: 7,
+        title: "Save the Hero",
+        duration: "4:33"
+      },
+      {
+        id: 8,
+        title: "Single Laddies (Put a Ring On It)",
+        duration: "4:16"
+      },
+      {
+        id: 9,
+        title: "Radio",
+        duration: "3:38"
+      },
+      {
+        id: 10,
+        title: "Diva",
+        duration: "3:20"
+      },
+      {
+        id: 11,
+        title: "Sweet Dreams",
+        duration: "3:28"
+      }
+    ]
+  },
+  {
+    artist: "Beyonce",
+    album: "Beyonce",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Beyonc%C3%A9_-_Beyonc%C3%A9.svg/1200px-Beyonc%C3%A9_-_Beyonc%C3%A9.svg.png",
+    tracks: [
+      {
+        id: 1,
+        title: "Pretty Hurts",
+        duration: "4:17"
+      },
+      {
+        id: 2,
+        title: "Haunted",
+        duration: "6:09"
+      },
+      {
+        id: 3,
+        title: "Drunk In Love",
+        duration: "5:23"
+      },
+      {
+        id: 4,
+        title: "Blow",
+        duration: "5:09"
+      },
+      {
+        id: 5,
+        title: "No Angel",
+        duration: "3:48"
+      },
+      {
+        id: 6,
+        title: "Partition",
+        duration: "5:19"
+      },
+      {
+        id: 7,
+        title: "Jealous",
+        duration: "3:04"
+      },
+      {
+        id: 8,
+        title: "Rocket",
+        duration: "6:31"
+      },
+      {
+        id: 9,
+        title: "Mine",
+        duration: "6:18"
+      },
+      {
+        id: 10,
+        title: "XO",
+        duration: "3:35"
+      },
+      {
+        id: 11,
+        title: "Flawless",
+        duration: "4:10"
+      },
+      {
+        id: 12,
+        title: "Superpower",
+        duration: "4:36"
+      },
+      {
+        id: 13,
+        title: "Heaven",
+        duration: "3:50"
+      },
+      {
+        id: 14,
+        title: "Blue",
+        duration: "4:26"
+      }
+    ]
+  },
+  {
+    artist: "Beyonce",
+    album: "Lemonade",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/5/53/Beyonce_-_Lemonade_%28Official_Album_Cover%29.png",
+    tracks: [
+      {
+        id: 1,
+        title: "Pray You Catch Me",
+        duration: "3:16"
+      },
+      {
+        id: 2,
+        title: "Hold Up",
+        duration: "3:41"
+      },
+      {
+        id: 3,
+        title: "Don't Hurt Yourself",
+        duration: "3:53"
+      },
+      {
+        id: 4,
+        title: "Sorry",
+        duration: "3:52"
+      },
+      {
+        id: 5,
+        title: "6 Inch",
+        duration: "4:20"
+      },
+      {
+        id: 6,
+        title: "Daddy Lessons",
+        duration: "4:48"
+      },
+      {
+        id: 7,
+        title: "Love Drought",
+        duration: "3:57"
+      },
+      {
+        id: 8,
+        title: "Sandcastles",
+        duration: "3:02"
+      },
+      {
+        id: 9,
+        title: "Forward",
+        duration: "1:19"
+      },
+      {
+        id: 10,
+        title: "Freedom",
+        duration: "4:49"
+      },
+      {
+        id: 11,
+        title: "All Night",
+        duration: "5:22"
+      },
+      {
+        id: 12,
+        title: "Formation",
+        duration: "3:26"
+      }
+    ]
+  }
+];
+
+export default data;
